62d4c98b2c1d90366e0f9610429255f9961bcd23
Paul <38801847+plmaida@users.noreply.github.com>
2019-02-09 16:53:16 -0500

    update

--- Clustering/titanic_clustering.py
+++ Clustering/titanic_clustering.py
@@ -1,5 +1,4 @@
-# Setting up a dendogram for part a
-
+import json
 from sklearn import cluster, datasets
 from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
@@ -9,7 +8,43 @@ import optparse
 import sys
 import random
 
+filename = open("titanic.json")
+
+features = json.load(filename)
+
 dataset = []
+target = []
+
+companion = []
+temprow = []
+temptarg = []
+
+print(features)
+for i in range(0, len(features)):
+	feat = [features[i]['Age'],features[i]['Embarked'],features[i]['Fare'],features[i]['Sex'],float(features[i]['SiblingsAndSpouses'])+float(features[i]['ParentsAndChildren']), float(features[i]["Survived"])] #include survived in the features
+	temprow.append(feat)
+	#temptarg.append(targ)
+	for b in range(0, 5):
+		if (temprow[i][b] == "S"):
+			temprow[i][b] = 0
+		elif (temprow[i][b] == "Q"):
+			temprow[i][b] = 1
+		elif (temprow[i][b] == "C"):
+			temprow[i][b] = 2
+		elif (temprow[i][b] == "male"):
+			temprow[i][b] = 0
+		elif (temprow[i][b] == "female"):
+			temprow[i][b] = 1
+		#elif(dataset[i][b] == ""):
+		#	dataset[i][b] = 1
+	try:
+		for b in range(0, 6):
+			temprow[i][b] = float(temprow[i][b])
+
+	except:
+		continue
+	dataset.append(temprow[i])
+	target.append(temptarg)
 
 def euclid(data, cent):
 	distance = 0.0
@@ -18,48 +53,6 @@ def euclid(data, cent):
 	eucdist = distance ** 0.5
 	return eucdist
 
-def processText(data):
-	data = data.strip()
-	print("DEBUG: ", data)
-	fields = data.split(',')
-	print("behold, an ordinary python list: ", fields)
-	for field in fields:
-		print("Here's the value of my field variable: ", field)
-	fields = np.float64(fields)
-	dataset.append(fields)
-
-#create a new option parser
-parser = optparse.OptionParser()
-#add an option to look for the -f (question 3)
-parser.add_option('-f', '--file', dest='fileName', help='file name to read from')
-
-#get the options entered by the user at the terminal
-(options, others) = parser.parse_args()
-
-usingFile = False
-
-#inspect the options entered by the user!
-if options.fileName is None:
-	print("DEBUG: the user did not enter the -f option")
-else:
-	print("DEBUG: the user entered the -f option")
-	usingFile = True
-
-if(usingFile == True):
-	#attempt to open and read out of the file
-	print("DEBUG: the file name entered was: ", options.fileName)
-	file = open(options.fileName, "r") # "r" means we are opening the file for reading
-	#write a loop that will read one line from the file at a time..
-	for line in file:
-		processText(line)
-
-
-else:
-	#read from standard input (shellexample3_demo.py)
-	print("DEBUG: will read from standard input instead")
-	for line in sys.stdin.readlines():
-        	processText(line)
-
 dummy = input("Press Enter to show non normalized data")
 dataset = np.array(dataset)
 print(dataset)
@@ -67,32 +60,27 @@ dummy = input("Press Enter to normalize")
 data_norm = []
 min_data = [] #previously was a large number, now is the age in the first row of your table
 max_data = []
-for l in range(0, 2):
+for l in range(0, 6):
 	min_data.append(dataset[0][l])
 	max_data.append(dataset[0][l])
 
 for a in range(0, len(dataset)):
-	for l in range(0,2):
+	for l in range(0, 6):
 		if dataset[a][l] < min_data[l]:
 			min_data[l] = dataset[a][l]
 		if dataset[a][l] > max_data[l]:
 			max_data[l] = dataset[a][l]
 
-
 #use a loop to normalize every value in the table by scaling it to the range 0..1
 for a in range(0, len(dataset)):
 	temp_norm = []
-	for l in range(0,2):
+	for l in range(0,6):
 		norm = (dataset[a][l] - min_data[l])/(max_data[l] - min_data[l])
 		norm = np.float64(norm)
 		temp_norm.append(norm)
 	data_norm.append(temp_norm)
 
 dataset = np.array(data_norm)
-
-print(dataset)
-
-print(np.shape(dataset))
 
 dummy = input("Press Enter to show dendrogram")
 from scipy.cluster.hierarchy import dendrogram, linkage
@@ -103,26 +91,23 @@ dendrogram(Z, leaf_rotation=50, leaf_font_size=8)
 plt.title("Dendrogram from Scipy")
 plt.xlabel("Sample")
 plt.ylabel("Distance")
-plt.savefig("Dendrogram_Q1.jpg")
+plt.savefig("Dendrogram_Q2.jpg")
 
 plt.figure(2)
-dendrogram(Z, truncate_mode='lastp', p=3, leaf_rotation=50, leaf_font_size=8, show_contracted = True)
+dendrogram(Z, truncate_mode='lastp', p=6, leaf_rotation=50, leaf_font_size=8, show_contracted = True)
 
 plt.title("Dendrogram from Scipy")
 plt.xlabel("Sample")
 plt.ylabel("Distance")
-plt.savefig("Dendrogram with 3 lines.jpg")
+plt.savefig("Dendrogram with 6 lines Q2.jpg")
 plt.show()
 
 
 dummy = input("Press Enter to complete clustering")
-k = input("Enter the amount of clusters based on the dendrogram (for this particular dataset use 3: ")
+k = input("Enter the amount of clusters based on the dendrogram (for this particular dataset use 6: ")
 k = np.int32(k)
-
 #after seeing the dendrogram any number can be picked for the k means analysis.
-#the one chosen based on the dendrogram for this data set should be 3
-
-
+#the one chosen based on the dendrogram for this data set should be 6
 
 plt.figure(3)
 centroid = []
@@ -132,11 +117,6 @@ for i in range(0, k):	#use three based on the dendrogram shown in part a
 	centroid.append(dataset[random.randint(0,len(dataset))])
 	point.append([])
 	point[i].append(centroid[i])
-#point.append(centroid)
-		#cluster.kmeans(n_clusters = k, max_iter=100, n_init=100, dataset)
-
-print(centroid)
-print(point)
 
 it = [5, 10, 100]
 for b in range(0, 101):
@@ -149,6 +129,7 @@ for b in range(0, 101):
 		dist = []
 		for center in range(0, k): # calculate a list of distances for each data point and centroid
 			dist.append(euclid(dataset[a], centroid[center]))
+
 		for clt in range(0, k):	# put it in the correct cluster
 			if (min(dist) == dist[clt]):
 				cltlist[clt].append(dataset[a])
@@ -159,17 +140,56 @@ for b in range(0, 101):
 		for clt in range(0, k):
 			point[clt].append(centroid[clt])
 			print("The size of the cluster at iteration", b, "is: ", len(cltlist[clt]))
-		print(point)
-
 
 
 	print("iteration", b)
 for i in range(0, k):
 	print("The final amount of points in cluster", i, "is: ", len(cltlist[i]))
+
 plt.title("Moving Centroids")
 
-for i in range (0, k):
-	plt.scatter(point[i][:][0], point[i][:][1]) #plot the moving centroids. From the dataset it initializes quickly and therefore there are only a few data points
-plt.xlabel("x values")
-plt.ylabel("y values")
-plt.savefig("kmeans_points.jpg")
+plots = []
+
+names=['Age', 'Embarked', 'Fare', 'Sex', 'Companions', 'Survived']
+print(np.shape(point))
+print(type(point))
+point = np.array(point)
+print(type(point))
+e=1
+i = 0
+
+for i in range(0, 6):
+	if i<5:
+		plt.subplot(2, 3, e)
+	 # shorthand for k = k +1#subplot creates panels
+	#taking pieces of arrays (lists, tables) is called
+	#"array slicing" -we've done this manually in the past
+	#there's a shorthand syntax for array slicing:
+		for d in range(0, k):			#http://structure.usc.edu/numarray/node26.html
+			plt.scatter(point[d,:,i], point[d,:,5])
+		plt.xlabel(names[i])
+		plt.ylabel(names[5])
+		e+=1
+
+plt.savefig("kmeans moving values for Q2.jpg")
+
+c_survive = "blue"
+c_dead = "red"
+e=1
+colour = []
+
+plt.figure(4)
+plt.title("Clusters Survivors/Dead")
+for i in range(5):
+	#for j in range(5):
+	if i<5:
+		plt.subplot(2, 3, e) #subplot creates panels
+			#taking pieces of arrays (lists, tables) is called
+			#"array slicing" -we've done this manually in the past
+			#there's a shorthand syntax for array slicing:
+			#http://structure.usc.edu/numarray/node26.html
+		plt.scatter(dataset[:,i], dataset[:,5])
+		plt.xlabel(names[i])
+		plt.ylabel(names[5])
+		e+=1 # shorthand for k = k +1
+plt.savefig("kmeans clusters for Q2.jpg")
